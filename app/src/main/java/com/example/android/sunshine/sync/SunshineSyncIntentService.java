9929b2a473348cbdd6faacc1d382a2853ce0deb3
saurabhjn76 <saurabhjn76@gmail.com>
2017-02-15 12:47:09 +0530

    sending data from app to wearable

--- app/src/main/java/com/example/android/sunshine/sync/SunshineSyncIntentService.java
+++ app/src/main/java/com/example/android/sunshine/sync/SunshineSyncIntentService.java
@@ -17,19 +17,71 @@ package com.example.android.sunshine.sync;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.os.Bundle;
+import android.provider.CalendarContract;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.wearable.DataApi;
+import com.google.android.gms.wearable.PutDataMapRequest;
+import com.google.android.gms.wearable.PutDataRequest;
+import com.google.android.gms.wearable.Wearable;
+
+// thanks to http://stackoverflow.com/questions/25413162/sending-data-to-android-wear-device
 /**
  * An {@link IntentService} subclass for handling asynchronous task requests in
  * a service on a separate handler thread.
  */
-public class SunshineSyncIntentService extends IntentService {
+public class SunshineSyncIntentService extends IntentService implements GoogleApiClient.ConnectionCallbacks,GoogleApiClient.OnConnectionFailedListener{
 
+    private static final String TAG = "SendDataService";
+    GoogleApiClient mGoogleApiClient;
     public SunshineSyncIntentService() {
         super("SunshineSyncIntentService");
     }
 
     @Override
+    public void onStart(Intent intent, int startId) {
+        super.onStart(intent, startId);
+    }
+
+    @Override
     protected void onHandleIntent(Intent intent) {
         SunshineSyncTask.syncWeather(this);
+        PutDataMapRequest dataMap = PutDataMapRequest.create("/events");
+        dataMap.getDataMap().putFloat("LowTemp",15);
+        dataMap.getDataMap().putFloat("HighTemp",28);
+      //  dataMap.getDataMap().putStringArray("events", eventStrings);
+        PutDataRequest request = dataMap.asPutDataRequest();
+        PendingResult<DataApi.DataItemResult> pendingResult = Wearable.DataApi
+                .putDataItem(mGoogleApiClient, request);
+        mGoogleApiClient.disconnect();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        mGoogleApiClient=new GoogleApiClient.Builder(this).addConnectionCallbacks(this).addOnConnectionFailedListener(this).addApi(Wearable.API).build();
+        mGoogleApiClient.connect();
+        return super.onStartCommand(intent, flags, startId);
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.d(TAG, "onConnected: " + bundle);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d(TAG, "onConnectionSuspended: " + i);
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d(TAG, "onConnectionFailed: " + connectionResult);
     }
 }
